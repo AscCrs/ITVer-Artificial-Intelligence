e77f50e3d20740c918cc984b41a7b72b16cbeb3e
AscCrs <cristoe2003@gmail.com>
2025-04-14 18:53:25 -0600

    Se agrego la clase del problema principal para planeacion de rutas

---
+++ Proyectos/Unidad-2/02PlaneacionRutas/src/planeacionrutas/PlaneacionRutas.java
@@ -0,0 +1,29 @@
+package planeacionrutas;
+
+import grafo.Grafo;
+import  static espaciodeestados.EstrategiaBusqueda.*;
+
+/**
+ *
+ * @author rafael
+ */
+public class PlaneacionRutas {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    String archivo = PlaneacionRutas.class.
+            getResource("/problemas/Ejemplo-mundo-bloques.graph").getPath().replace("%20", " ");
+    Grafo grafo = new Grafo(archivo);
+    EstadoNodo estadoInicial = new EstadoNodo(grafo.getNodos().get("C-A B"));
+    EstadoNodo estadoMeta = new EstadoNodo(grafo.getNodos().get("A-B-C"));
+    System.out.println(estadoInicial);
+    System.out.println(estadoMeta);
+    ProblemaPlaneacion problema =  new ProblemaPlaneacion(grafo,estadoInicial,estadoMeta);
+    long tiempo = System.currentTimeMillis();
+    problema.buscarSolucion(BUSQUEDA_POR_PROFUNDIDAD_LIMITADA, SIN_REPETICION, 3);
+    System.out.println(System.currentTimeMillis() - tiempo);
+  }
+
+}
